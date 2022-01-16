3891816224bec978d843c08a364820bf481f7e43
Your Name <mohamedabotyear@gmail.com>
2022-01-17 01:25:09 +0200

    Credentials

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBook.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public string Favor { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
